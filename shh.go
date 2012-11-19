ebd17a5f71c1eb6ea37de123b27c184ee1bb5a29
Edward Muller <edwardam@interlix.com>
2012-11-19 03:17:00 +0000

    go fmt

--- shh.go
+++ shh.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"shh/mm"
 	"time"
-  "shh/mm"
 )
 
 func writeOut(measurements chan *mm.Measurement) {
@@ -24,10 +24,10 @@ func pollLoad(now time.Time, measurements chan *mm.Measurement) {
 	measurements <- &mm.Measurement{now, "load.1m", fields[0]}
 	measurements <- &mm.Measurement{now, "load.5m", fields[1]}
 	measurements <- &mm.Measurement{now, "load.15m", fields[2]}
-  entities := bytes.Split(fields[3],[ ]byte("/"))
-  measurements <- &mm.Measurement{now, "scheduling.entities.executing", entities[0]}
-  measurements <- &mm.Measurement{now, "scheduling.entities.total", entities[1]}
-  measurements <- &mm.Measurement{now, "pid.last", fields[4]}
+	entities := bytes.Split(fields[3], []byte("/"))
+	measurements <- &mm.Measurement{now, "scheduling.entities.executing", entities[0]}
+	measurements <- &mm.Measurement{now, "scheduling.entities.total", entities[1]}
+	measurements <- &mm.Measurement{now, "pid.last", fields[4]}
 	return
 }
 
@@ -38,6 +38,6 @@ func main() {
 	go writeOut(measurements)
 	for now := range ticks {
 		measurements <- &mm.Measurement{now, "tick", []byte("true")}
-    go pollLoad(now, measurements)
+		go pollLoad(now, measurements)
 	}
 }
