15ca3e9835f2987b5f00f3211facdd9dbef78978
Edward Muller <edwardam@interlix.com>
2013-01-15 15:52:47 +0000

    support SHH_INTERVAL and default to 10s

--- shh.go
+++ shh.go
@@ -4,7 +4,13 @@ import (
 	"fmt"
 	"github.com/freeformz/shh/mm"
 	"github.com/freeformz/shh/pollers"
+	"log"
+	"os"
 	"time"
+)
+
+const (
+	DefaultInterval = "10s"
 )
 
 func writeOut(measurements chan *mm.Measurement) {
@@ -13,10 +19,29 @@ func writeOut(measurements chan *mm.Measurement) {
 	}
 }
 
+func getDuration() time.Duration {
+	interval := os.Getenv("SSH_INTERVAL")
+
+	if interval == "" {
+		interval = DefaultInterval
+	}
+
+	duration, err := time.ParseDuration(interval)
+
+	if err != nil {
+		log.Fatal("unable to parse SHH_INTERVAL: " + interval)
+	}
+
+	return duration
+}
+
 func main() {
 	measurements := make(chan *mm.Measurement, 100)
-	duration, _ := time.ParseDuration("5s")
+
+	duration := getDuration()
+	fmt.Sprintf("duration=%s", duration)
 	ticks := time.Tick(duration)
+
 	go writeOut(measurements)
 
 	mp := pollers.NewMultiPoller()
