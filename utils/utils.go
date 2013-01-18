aaac543f9b2127da5892a11abcd344fa72ee6021
Edward Muller <edwardam@interlix.com>
2013-01-18 03:50:53 +0000

    make this easy

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,15 @@
 package utils
 
 import (
+	"os"
 	"strconv"
 )
 
 func Ui64toa(val uint64) string {
 	return strconv.FormatUint(val, 10)
+}
+
+func Exists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
 }
