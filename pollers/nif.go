ba4992f6b6ec9d55334d281b70b8bfcce69d1cca
Edward Muller <edwardam@interlix.com>
2013-02-01 11:53:28 -0800

    Sometimes the counter is so big that it can't be split on a space.

--- pollers/nif.go
+++ pollers/nif.go
@@ -29,33 +29,36 @@ func NewNetworkInterfacePoller(measurements chan<- *mm.Measurement) NetworkInter
 func (poller NetworkInterface) Poll(tick time.Time) {
 
 	for line := range utils.FileLineChannel(DEVICE_FILE) {
-		fields := strings.Fields(line)
-		if strings.HasSuffix(fields[0], ":") {
-			device := fields[0]
-			nakedDevice := device[:len(device)-1]
-			idx := sort.SearchStrings(devices, nakedDevice)
-			if idx < len(devices) && devices[idx] == nakedDevice {
-				// It's a device we want to gather metrics for
-
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "receive", "bytes"}, utils.Atouint64(fields[1])}
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "receive", "packets"}, utils.Atouint64(fields[2])}
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "receive", "errors"}, utils.Atouint64(fields[3])}
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "receive", "dropped"}, utils.Atouint64(fields[4])}
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "receive", "errors", "fifo"}, utils.Atouint64(fields[5])}
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "receive", "errors", "frame"}, utils.Atouint64(fields[6])}
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "receive", "compressed"}, utils.Atouint64(fields[7])}
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "receive", "multicast"}, utils.Atouint64(fields[8])}
-
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "transmit", "bytes"}, utils.Atouint64(fields[9])}
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "transmit", "packets"}, utils.Atouint64(fields[10])}
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "transmit", "errors"}, utils.Atouint64(fields[11])}
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "transmit", "dropped"}, utils.Atouint64(fields[12])}
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "transmit", "errors", "fifo"}, utils.Atouint64(fields[13])}
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "transmit", "errors", "collisions"}, utils.Atouint64(fields[14])}
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "transmit", "errors", "carrier"}, utils.Atouint64(fields[15])}
-				poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{nakedDevice, "transmit", "compressed"}, utils.Atouint64(fields[16])}
-
+		fields := strings.FieldsFunc(line, func(s rune) bool {
+			if s == ':' || s == ' ' {
+				return true
 			}
+			return false
+		})
+
+		device := fields[0]
+		idx := sort.SearchStrings(devices, device)
+		if idx < len(devices) && devices[idx] == device {
+			// It's a device we want to gather metrics for
+
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "receive", "bytes"}, utils.Atouint64(fields[1])}
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "receive", "packets"}, utils.Atouint64(fields[2])}
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "receive", "errors"}, utils.Atouint64(fields[3])}
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "receive", "dropped"}, utils.Atouint64(fields[4])}
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "receive", "errors", "fifo"}, utils.Atouint64(fields[5])}
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "receive", "errors", "frame"}, utils.Atouint64(fields[6])}
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "receive", "compressed"}, utils.Atouint64(fields[7])}
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "receive", "multicast"}, utils.Atouint64(fields[8])}
+
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "transmit", "bytes"}, utils.Atouint64(fields[9])}
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "transmit", "packets"}, utils.Atouint64(fields[10])}
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "transmit", "errors"}, utils.Atouint64(fields[11])}
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "transmit", "dropped"}, utils.Atouint64(fields[12])}
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "transmit", "errors", "fifo"}, utils.Atouint64(fields[13])}
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "transmit", "errors", "collisions"}, utils.Atouint64(fields[14])}
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "transmit", "errors", "carrier"}, utils.Atouint64(fields[15])}
+			poller.measurements <- &mm.Measurement{tick, poller.Name(), []string{device, "transmit", "compressed"}, utils.Atouint64(fields[16])}
+
 		}
 	}
 }
