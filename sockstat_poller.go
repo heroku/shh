5756f0ebbb33a0f528d86cdc7352154c6f08915a
Andrew Gwozdziewycz <git@apgwoz.com>
2014-07-17 00:43:24 -0400

    Add a poller for sockstats
      - Supports filtering by protocol (first field in /proc/net/sockstats{,6}
      - Turned off by default

---
+++ sockstat_poller.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"strings"
+	"time"
+)
+
+var (
+	SOCKSTAT4 = "/proc/net/sockstat"
+	SOCKSTAT6 = "/proc/net/sockstat6"
+)
+
+type SockStat struct {
+	measurements chan<- *Measurement
+	files []string
+	Protocols []string
+}
+
+func NewSockStatPoller(measurements chan<- *Measurement, config Config) SockStat {
+	var includeV6, includeV4 bool
+	for _, proto := range config.SockStatProtos {
+		if strings.HasSuffix(proto, "6") {
+			includeV6 = true
+		} else {
+			includeV4 = true
+		}
+	}
+
+	filesToPoll := make([]string, 0)
+	if includeV4 {
+		filesToPoll = append(filesToPoll, SOCKSTAT4)
+	}
+	if includeV6 {
+		filesToPoll = append(filesToPoll, SOCKSTAT6)
+	}
+
+	return SockStat{
+		measurements: measurements,
+		files:        filesToPoll,
+	  Protocols:    config.SockStatProtos,
+	}
+}
+
+// http://www.kernel.org/doc/Documentation/filesystems/proc.txt (section 1.4)
+func (poller SockStat) Poll(tick time.Time) {
+	for _, file := range poller.files {
+		for line := range FileLineChannel(file) {
+			fields := Fields(line)
+			proto := fields[0]
+
+			if SliceContainsString(poller.Protocols, proto) && len(fields) > 1 {
+				for i := 1; i+1 < len(fields); i += 2 {
+					poller.measurements <- &Measurement{tick, poller.Name(), []string{proto, fields[i]}, Atouint64(fields[i+1])}
+				}
+			}
+		}
+	}
+}
+
+func (poller SockStat) Name() string {
+	return "sockstat"
+}
+
+func (poller SockStat) Exit() {}
