97e29170e6c3fd956457d3fecd58bbdae13b2d54
Andrew Gwozdziewycz <git@apgwoz.com>
2014-10-13 13:54:57 -0400

    Handle EOF with retry

--- librato_output.go
+++ librato_output.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -178,6 +179,9 @@ func (out *Librato) send(ctx Slog, payload []byte) (retry bool, e error) {
 		} else if strings.Contains(err.Error(), "timeout awaiting response") {
 			retry = false
 			e = nil
+		} else if err == io.EOF {
+			retry = true
+			e = fmt.Errorf("Backing off due to EOF")
 		} else {
 			ctx.FatalError(err, "doing request")
 		}
